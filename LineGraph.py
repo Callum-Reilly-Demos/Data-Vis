58ea5e0a877dc839142ebf14c90c8ff2a41c0097
Callum-Reilly-Demos <63015951+Callum-Reilly-Demos@users.noreply.github.com>
2021-08-18 15:46:56 +0100

    Update LineGraph.py

--- LineGraph.py
+++ LineGraph.py
@@ -1,13 +1,14 @@
 from bokeh.plotting import figure, show
 
-
+##  defines values on each axis
 x = [1, 2, 3, 4, 5]
 y = [7, 1, 10, 15, 5]
 
-## Create a new plot with a title and axis labels 
-
+## Create a new graph plot with a title and axis labels 
 plot = figure(title="An Example Line Graph", x_axis_label='X Axis', y_axis_label='Y axis')
 
+#plots the line on the graph with a legend label
 plot.line(x, y, legend_label="A Key Example.", line_width=5)
 
+#shows the plot in the browser
 show(plot)
