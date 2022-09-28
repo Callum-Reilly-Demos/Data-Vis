2e34cfcf21b458416a8cab3cf53c089d32fc9ccf
Callum-Reilly-Demos <63015951+Callum-Reilly-Demos@users.noreply.github.com>
2022-09-28 23:53:46 +0100

    Create Typecasting-better.py

---
+++ Typecasting-better.py
@@ -0,0 +1,4 @@
+
+#you can typecast on the same line by placing the data type desired infront of the input function
+Birth_Year = int(input('What year were you born? '))
+print('In 2050, you will be', 2050 - Birth_Year, 'years old.')
